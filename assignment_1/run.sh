bbd49374231efd8e615c136652c92d092fe4165e
fayepsr <foteini.psrr@gmail.com>
2021-10-05 21:57:22 +0200

    export plots

--- assignment_1/run.sh
+++ assignment_1/run.sh
@@ -4,13 +4,18 @@ FOLDER=results
 
 
 if [ -d "$FOLDER" ]; then #check if file exists
-    echo "$FOLDER exists."
+    echo "$FOLDER exists." #delete all previous results in folder
     rm $FOLDER/*
 fi
 
 counter=10 
 while [ "$counter" -gt 0 ]; do
-    python main.py 
+    python main.py -st file -se pickle -m store test.txt #this is a test call
     counter=$(( counter - 1 ))
     #python .test/plot.py
 done
+
+# all txt files in result folder will be plotted
+# !!! it will work when times will be written in results/*txt files !!!
+# TODO: change file path ... 
+python test/export_plots.py
---
+++ assignment_1/test/export_plots.py
@@ -0,0 +1,35 @@
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    try:
+        txt_files = glob.glob("results/*.txt")
+        found_files = 0
+
+        for txt in txt_files:          
+            datas = pd.read_csv(txt, delimiter=';', header=None)
+            elapsed_time = datas.loc[:, 0] - datas.loc[:, 1]
+            
+            plot_title = txt.removesuffix(".txt")
+            plot_title = plot_title.removeprefix("results/")
+            plot_title = plot_title.capitalize()
+
+            plt.figure(figsize=(8, 3))
+            plt.title(plot_title)
+            plt.plot(elapsed_time)
+            plt.ylabel('Time elapsed')
+
+            png_name = txt.removesuffix(".txt")
+            plt.savefig(png_name + ".png")
+
+            found_files += 1
+
+        if(found_files == 0 ):
+            print("No txt files found in results folder")
+
+    except Exception as e:
+       print(e)
+
+
